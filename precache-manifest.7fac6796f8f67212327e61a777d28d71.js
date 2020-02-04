aea696bc13b3b525d3f82b240ab1d5c8a91af92c
Danielle Burrage <burragedanielle@gmail.com>
2020-02-03 19:10:52 -0600

    Updates

---
+++ precache-manifest.7fac6796f8f67212327e61a777d28d71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c808fbd30bc07f57b6a1356d05346cdb",
+    "url": "/memory-game-curb/index.html"
+  },
+  {
+    "revision": "5a35ec919cc0d827cdf7",
+    "url": "/memory-game-curb/static/css/main.059d8acb.chunk.css"
+  },
+  {
+    "revision": "79a442cde1b398a798d2",
+    "url": "/memory-game-curb/static/js/2.f73c2ac4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/memory-game-curb/static/js/2.f73c2ac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a35ec919cc0d827cdf7",
+    "url": "/memory-game-curb/static/js/main.f93c32a4.chunk.js"
+  },
+  {
+    "revision": "4a72ed292aa09377cfe4",
+    "url": "/memory-game-curb/static/js/runtime-main.6d28f72b.js"
+  }
+]);
